bae19394fcefb7d06e394972f141ec713c48dc61
AkiraTenchi <jimjan2002@gmail.com>
2021-08-25 16:29:26 +0200

    implementet BRK and first tests

--- src/cpu.rs
+++ src/cpu.rs
@@ -21,27 +21,53 @@ impl CPU{
             self.program_counter += 1;
 
             match opscode {
-            0xA9 => {
-                let param = program[self.program_counter as usize];
-                self.program_counter += 1;
-                self.register_a = param;
+                0xA9 => {
+                    let param = program[self.program_counter as usize];
+                    self.program_counter += 1;
+                    self.register_a = param;
 
-                //check if register a is 0 and if it is we set zero flag to 1 else we set it to 0
-                if self.register_a == 0 {
-                    self.status = self.status | 0b0000_0010;
-                } else {
-                    self.status = self.status & 0b1111_1101;
+                    //check if register a is 0 and if it is we set zero flag to 1 else we set it to 0
+                    if self.register_a == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    //check if the negativ bit of register a is set if it is we set the negative bit of the status 
+                    if self.register_a & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
                 }
 
-                //check if the negativ bit of register a is set if it is we set the negative bit of the status 
-                if self.register_a & 0b1000_0000 != 0 {
-                    self.status = self.status | 0b1000_0000;
-                } else {
-                    self.status = self.status & 0b0111_1111;
+                0x00 => {
+                    return;
                 }
-            }
+                
                 _ => todo!()
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_0x49_lda_immidiate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 }
