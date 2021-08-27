969622c4753af5552844c36b8cdd761d478ba57a
AkiraTenchi <jimjan2002@gmail.com>
2021-08-27 02:28:22 +0200

    implementet inx and tests

--- src/cpu.rs
+++ src/cpu.rs
@@ -29,6 +29,12 @@ impl CPU {
         self.update_negative_flag(self.register_x);
     }
 
+    fn inx(&mut self){
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_flag(self.register_x);
+        self.update_negative_flag(self.register_x);
+    }
+
     fn update_zero_flag(&mut self, value: u8){
         //check if register a is 0 and if it is we set zero flag to 1 else we set it to 0
         if value == 0 {
@@ -62,6 +68,8 @@ impl CPU {
                 }
 
                 0xAA => self.tax(),
+
+                0xE8 => self.inx(),
 
                 0x00 => return,
 
@@ -105,4 +113,19 @@ mod tests {
         cpu.interpret(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
+
+    #[test]
+    fn test_lda_txa_inx() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        assert_eq!(cpu.register_x, 0xc1)
+    }
+ 
+     #[test]
+     fn test_inx_overflow() {
+         let mut cpu = CPU::new();
+         cpu.register_x = 0xff;
+         cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+         assert_eq!(cpu.register_x, 1)
+     }
 }
