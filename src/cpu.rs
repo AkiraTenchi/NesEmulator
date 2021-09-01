e1998bdb313d926e8289f37a71e23889b879912e
AkiraTenchi <jimjan2002@gmail.com>
2021-09-02 00:46:57 +0200

    fixed tests for new memory functionality

--- src/cpu.rs
+++ src/cpu.rs
@@ -34,6 +34,7 @@ impl CPU {
     }
 
     fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        //shifting 8 bits to the right
         let hi = (data >> 8) as u8;
         let lo = (data & 0xff) as u8;
         self.mem_write(pos, lo);
@@ -127,7 +128,7 @@ mod tests {
     #[test]
     fn test_0x49_lda_immidiate_load_data() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 0x05);
         assert!(cpu.status & 0b0000_0010 == 0b00);
         assert!(cpu.status & 0b1000_0000 == 0);
@@ -136,37 +137,35 @@ mod tests {
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
         let mut cpu = CPU::new();
-        cpu.register_a = 10;
-        cpu.interpret(vec![0xaa, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x0a, 0xaa, 0x00]);
         assert_eq!(cpu.register_x, 10)
     }
 
     #[test]
     fn test_0xaa_txa_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
     #[test]
     fn test_lda_txa_inx() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
         assert_eq!(cpu.register_x, 0xc1)
     }
  
      #[test]
      fn test_inx_overflow() {
          let mut cpu = CPU::new();
-         cpu.register_x = 0xff;
-         cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+         cpu.load_and_run(vec![0xa9, 0xff, 0xaa, 0xe8, 0xe8, 0x00]);
          assert_eq!(cpu.register_x, 1)
      }
 }
